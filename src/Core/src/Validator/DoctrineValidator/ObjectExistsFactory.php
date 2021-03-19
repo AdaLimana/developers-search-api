15aadd7d77b9e420ee7e9037131653269f231025
Adair J. Limana <ada_limana@yahoo.com.br>
2021-03-19 20:38:46 -0300

    feat(Core]DoctrineValidator): criadas as classes para usar na validação do doctrine, essas classes são uma cópia pois pertencem a um pacote maior, o qual foi criado para o laminas-mvc, no entanto, sozinhas, elas funcionam também para o mezzio, nao obrigando a instalar todas as outra dependências desnecessárias

---
+++ src/Core/src/Validator/DoctrineValidator/ObjectExistsFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Core\Validator\DoctrineValidator\Factory;
+
+use Interop\Container\ContainerInterface;
+use Core\Validator\DoctrineValidator\ObjectExists;
+
+/**
+ * Factory for creating ObjectExists instances
+ *
+ * @license MIT
+ * @link    http://www.doctrine-project.org/
+ * @since   1.3.0
+ * @author  Fabian Grutschus <f.grutschus@lubyte.de>
+ */
+class ObjectExistsFactory extends AbstractValidatorFactory
+{
+    protected $validatorClass = ObjectExists::class;
+
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        $container = $this->container($container);
+
+        $repository = $this->getRepository($container, $options);
+
+        $validator = new ObjectExists($this->merge($options, [
+            'object_repository' => $repository,
+            'fields'            => $this->getFields($options),
+        ]));
+
+        return $validator;
+    }
+}
