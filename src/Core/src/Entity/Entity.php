21da16858c6701ec19b265802b7b87f0ec44bf85
Adair J. Limana <ada_limana@yahoo.com.br>
2021-03-19 18:58:53 -0300

    feat: criada a class Core\Entity\Entity a qual servirá de base para todas as entidades

---
+++ src/Core/src/Entity/Entity.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Core\Entity;
+
+/**
+ * Description of Entity
+ *
+ * @author adair
+ */
+class Entity {
+    
+    public function __set($key, $value) {
+        
+        $method = "set" . ucfirst($key);
+            
+        if(method_exists($this, $method)){
+            $this->$method($value);
+        }
+    }
+
+    public function setData($data){
+        
+        foreach ($data as $key => $value){
+            
+            $this->__set($key, $value);
+        }
+    }
+}
